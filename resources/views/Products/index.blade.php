d4b91c0181c66138bc6bdc386b782a0890dddea2
yanhasby <yanhasby@gmail.com>
2025-12-10 12:00:14 +0700

    perbaikan create blade

---
+++ resources/views/Products/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h2>Daftar Produk</h2>
+
+    <a href="{{ route('products.create') }}" class="btn btn-primary mb-3">Tambah Produk</a>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Nama</th>
+                <th>Harga</th>
+                <th>Stok</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($products as $product)
+            <tr>
+                <td>{{ $product->name }}</td>
+                <td>Rp {{ number_format($product->price, 0, ',', '.') }}</td>
+                <td>{{ $product->stock }}</td>
+                <td>
+                    <a href="{{ route('products.edit', $product->id) }}" class="btn btn-sm btn-warning">Edit</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
